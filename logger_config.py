3003ed793640eea461c80ac8ccb101fb014d3f7c
evya <evyatarsaar979@gmail.com>
2023-10-01 12:18:59 +0300

    changing the logger to be relative to path - which hopefully should make it write only to a single app.log file.

--- logger_config.py
+++ logger_config.py
@@ -1,4 +1,8 @@
 import logging
+import os
+
+# Determine the directory of the main script (usually app.py)
+main_script_directory = os.path.dirname(__file__)
 
 # Create a logger
 logger = logging.getLogger(__name__)
@@ -6,8 +10,11 @@ logger = logging.getLogger(__name__)
 # Create a formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# Create a file handler to write log messages to a file
-file_handler = logging.FileHandler('app.log')
+# Build the relative log file path
+relative_log_file = os.path.join(main_script_directory, 'app.log')
+
+# Create a file handler to write log messages to the relative log file path
+file_handler = logging.FileHandler(relative_log_file)
 file_handler.setLevel(logging.INFO)  # Set the log level for the file handler
 file_handler.setFormatter(formatter)
 
@@ -22,4 +29,3 @@ logger.addHandler(console_handler)
 
 # Set the log level for the logger (you can adjust this as needed)
 logger.setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
